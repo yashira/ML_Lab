abfd5801a9bebd56db7f7ab473238c47ac281f3f
Yashira <yashira.perera@novigi.com.au>
2020-04-18 20:50:29 +1000

    Adding data clensing for dataprocessing.

--- 1-DataProcessing/DataProcessing.py
+++ 1-DataProcessing/DataProcessing.py
@@ -16,3 +16,12 @@ print(x)
 print(lineSeperator)
 print("Dependent Vector\n")
 print(y)
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+print(lineSeperator)
+print("After Data clensing\n")
+print(x)
